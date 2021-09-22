2dc929a05c2c607a80df54f743510f8c36922cfa
pasz4 <58959408+pasz4@users.noreply.github.com>
2021-09-22 20:18:02 +0930

    Add files via upload

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: prossi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 18:05:14 by prossi            #+#    #+#             */
-/*   Updated: 2021/09/20 19:01:31 by prossi           ###   ########.fr       */
+/*   Updated: 2021/09/22 20:17:04 by prossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 int		ft_strlen(const char *str);
 int		ft_strncmp(const char *s1, char *s2, size_t n);
-char	*ft_strnstr(const char *s1, const char *s2, size_t len);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
 int		ft_tolower(int i);
 int		ft_toupper(int i);
