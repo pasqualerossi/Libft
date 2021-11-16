6fae0a8188099192682a07fdff12783736d59884
Pasquale Rossi <58959408+pasz4@users.noreply.github.com>
2021-11-16 13:55:42 +1030

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -10,13 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/* This function writes n zeroed bytes to the string s. If n is zero, ft_bzero 
- * does nothing. Another way of saying this is that we will be making a string 
- * of size n and filling each index position with a 0. We use our previously 
- * made ft_memset function to do this. Review ft_memset to understand how it 
- * works and notice the parameters we give it here when we call it. 
- * We set it ahead of time to fill whatever string at size n with 0s. */
-
 #include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
